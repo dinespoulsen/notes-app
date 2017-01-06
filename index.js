6b80dfd4127781b153b605287de7d94fd74d67c3
Dines Poulsen <dinespoulsen84@gmail.com>
2017-01-06 11:42:36 +0000

    Added form to index page

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
-var noteController = new NoteController();
-window.addEventListener("hashchange", noteController.showNoteForCurrentPage.bind(noteController))
+window.onload = function(){
+  var noteController = new NoteController();
+  window.addEventListener("hashchange", noteController.showNoteForCurrentPage.bind(noteController))
   noteController.createNote("This is a note that is longer than 20 characters.");
+  noteController.createNote("This is a note that is also longer than 20 characters.");
   noteController.insertHTML();
+};
